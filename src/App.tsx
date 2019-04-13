8b9731c3265715bb210a08ec89b9f6d252bb2211
jakub41 <lemiszewski@gmx.com>
2019-04-13 21:42:46 +0200

    Added Maze component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import logo from './logo.svg';
+import MazeCeil from './maze_ceil';
+
+class App extends React.Component {
+	public render() {
+		return <div />;
+	}
 }
 
 export default App;
