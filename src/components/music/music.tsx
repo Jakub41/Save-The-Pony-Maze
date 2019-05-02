2b8bbf025d58f714ff0fa5aeea32b8d2c88e38ac
jakub41 <lemiszewski@gmx.com>
2019-05-03 01:11:14 +0200

    Fix

--- src/components/music/music.tsx
+++ src/components/music/music.tsx
@@ -7,8 +7,8 @@ interface Props {}
 const Music = React.memo(({  }: Props) => {
 	return (
 		<div>
-			<iframe src="music/silence.mp3" allow="autoplay" id="audio" style={{ display: 'none' }} />
-			<audio id="music" autoPlay src="music/bundle.mp3" />
+			<iframe src="/music/silence.mp3" allow="autoplay" id="audio" style={{ display: 'none' }} />
+			<audio id="music" autoPlay src="/music/bundle.mp3" />
 		</div>
 	);
 });
